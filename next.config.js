cfca9d9841fc29af864c369bf5050d0129c55876
DidoNael <52351947+DidoNael@users.noreply.github.com>
2026-03-14 18:51:41 -0300

    fix: isolate build dir to .next_docker to prevent host conflicts (v1.8.8)

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  distDir: ".next_docker",
   experimental: {
     serverComponentsExternalPackages: ["@prisma/client", "bcryptjs"],
   },
