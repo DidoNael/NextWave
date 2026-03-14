5df3a7fb3b062af90e22e65f1d836dfae0402170
DidoNael <52351947+DidoNael@users.noreply.github.com>
2026-03-14 17:20:15 -0300

    fix: docker entrypoint flags and speed (v1.7.9)

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -11,5 +11,5 @@ echo "================================================"
 # Sincronizar schema (garante que as tabelas existam)
 echo ""
 echo "[1/2] Sincronizando schema do banco de dados..."
-npx prisma db push --skip-generate
+./node_modules/.bin/prisma db push --accept-data-loss
 exec node server.js
